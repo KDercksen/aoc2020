55c05a248663fba0f0c2c903a0e75b7cd6a5c7e7
KDercksen <mail@koendercksen.com>
2020-12-01 12:11:40 +0100

    oneliners!

--- one/one.py
+++ one/one.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from itertools import combinations
+
 with open("input_one.txt") as f:
     nums = list(map(int, f.readlines()))
 
-for i, num1 in enumerate(nums):
-    for num2 in nums[i + 1 :]:
-        if num1 + num2 == 2020:
-            print(num1 * num2)
+print([x * y for x, y in combinations(nums, 2) if x + y == 2020][0])
--- one/two.py
+++ one/two.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from itertools import combinations
+
 with open("input_one.txt") as f:
     nums = list(map(int, f.readlines()))
 
-for i, num1 in enumerate(nums):
-    for j, num2 in enumerate(nums[i + 1 :], start=i + 1):
-        for num3 in nums[j + 1 :]:
-            if num1 + num2 + num3 == 2020:
-                print(num1 * num2 * num3)
+print([x * y * z for x, y, z in combinations(nums, 3) if x + y + z == 2020][0])
